32c2335c9327e6593ae6b5604495e6b88fe8c17e
Brutalism1331 <brutalism.1331@gmail.com>
2021-08-01 13:23:00 -0700

    almost finished all chalange 1

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -95,8 +95,8 @@ const addValues = (arr, value) => {
 };
 
 const addNumbers = (num, arr, times, callback) => {
-  for (let i = 0; i < times; i++){
-    callback(arr,num);
+  for (let i = 0; i < times; i++) {
+    callback(arr, num);
   }
   return arr;
 };
@@ -123,7 +123,7 @@ const createList = (availableItems) => {
   let list = [];
 
   availableItems.forEach(item => {
-    if(item.available){
+    if (item.available) {
       list.push(item.name);
     }
   });
@@ -143,11 +143,25 @@ Iterate over the array using forEach to determine the output based on several ru
 
 Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
+// [x] / 3or5 = Fizz Buzz, if
+// [x] / 3 = Fizz, ifElse,
+// [x] / 5 = Buzz, ifElse,
+// [x] not / 3or5 = x, else
 
 const fizzbuzz = (arr) => {
-  // Solution code here...
-};
+  const newArr = arr.forEach(x => {
+    if (x % 3 === 0 && x % 5 === 0) {
+      return 'Fizz Buzz';
+    } else if (x % 3 === 0) {
+      return 'Fizz';
+    } else if (x % 5 === 0) {
+      return 'Buzz';
+    } else (x % 3 !== 0 || x % 5 !== 0);
+    return x;
 
+  });
+  return newArr;
+};
 /* ------------------------------------------------------------------------------------------------
 TESTS
 
@@ -199,7 +213,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
